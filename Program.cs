d14bc389122cffbdc09d89e26b17a636db288fcd
Vishwajeet121289 <rathorevishwajeetsingh@gmail.com>
2022-09-28 17:30:04 +0530

    Welcome to EmpWage problem

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace empWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
