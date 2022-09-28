3f6e16837c1a2fc92478c4ed455dfa901d00fcc1
Vishwajeet121289 <rathorevishwajeetsingh@gmail.com>
2022-09-28 19:00:02 +0530

    To Cal Daily Emp Wage

--- Program.cs
+++ Program.cs
@@ -10,14 +10,20 @@ namespace empWage
             int empPresent = 1;
             Random random = new Random();
             int EmpCheck = random.Next(0, 2);
+            int empHr;
+            int empRatePerHr = 20;
             if (EmpCheck == empPresent)
             {
-                Console.WriteLine("Employee Present");
+                Console.WriteLine("Employee is present");
+                empHr = 8;
             }
             else
             {
-                Console.WriteLine("Employee Absent");
+                Console.WriteLine("Employee is absent");
+                empHr = 0;
             }
+            int totalWage = empRatePerHr * empHr;
+            Console.WriteLine("Total wage is " + totalWage);
         }
     }
 }
