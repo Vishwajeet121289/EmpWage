4a3a10e69bea365a062ff4d01c581bd6ab956f8b
Vishwajeet121289 <rathorevishwajeetsingh@gmail.com>
2022-09-28 22:54:46 +0530

    EmpWage by SwitchCase

--- Program.cs
+++ Program.cs
@@ -4,29 +4,29 @@ namespace empWage
 {
     class Program
     {
+        public const int fullTimePresent = 1;
+        public const int partTimePresent = 2;
+        public const int empRatePerHr = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            int fulltimePresent = 1;
-            int parttimePresent = 2;
             Random random = new Random();
             int EmpCheck = random.Next(0, 3);
             int empHr;
             int empRatePerHr = 20;
-            if (EmpCheck == fulltimePresent)
+            switch(EmpCheck)
             {
-                Console.WriteLine("Employee is fulltime present");
-                empHr = 8;
-            }
-            else if (EmpCheck == parttimePresent)
-            {
-                Console.WriteLine("Employee is parttime present");
-                empHr = 8;
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-                empHr = 0;
+
+                case fullTimePresent:
+                    empHr = 8;
+                    break;
+                case partTimePresent:
+                    empHr = 4;
+                    break;
+                default:
+                    empHr = 0;
+                    break;
+
             }
             int totalWage = empRatePerHr * empHr;
             Console.WriteLine("Total wage is " + totalWage);
